73875e60a80eb019d34b2ae45cac6126aa95831f
Sourik Roy <91664367+Im-Rik@users.noreply.github.com>
2024-08-26 23:38:53 +0530

    Update subArrayProblems.cpp

--- Array/subArrayProblems.cpp
+++ Array/subArrayProblems.cpp
@@ -1,1 +1,24 @@
+//Given an array arr containing n integers and an integer k. Your task is to find the length of the longest Sub-Array with the sum of the elements equal to the given value k.
 
+//Brute -- Almost O(n^3) which can be done as O(n^2) at best
+
+int longestSubArrayBruteForce(const vector<int>& arr, int k) {
+    int n = arr.size();
+    int maxLength = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += arr[j];
+            if (sum == k) {
+                maxLength = max(maxLength, j - i + 1);
+            }
+        }
+    }
+
+    return maxLength;
+}
+
+
+
+//Better -- Using hashing 
